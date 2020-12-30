4178101218c6a71c6658a0f93e7d6dee57f0ba63
shinixsensei <crezoxplays@gmail.com>
2020-12-30 14:55:49 +0100

    Initial commit

---
+++ src/main/java/bugbot/Main.java
@@ -0,0 +1,25 @@
+package bugbot;
+
+import bugbot.config.Config;
+import bugbot.listeners.BugHandler;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static void main(String[] args) throws LoginException {
+
+        JDABuilder builder = JDABuilder.createDefault(Config.TOKEN);
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES);
+        builder.setBulkDeleteSplittingEnabled(false);
+        builder.setCompression(Compression.NONE);
+
+        builder.addEventListeners(new BugHandler());
+        builder.build();
+
+    }
+
+}
---
+++ src/main/java/bugbot/listeners/BugHandler.java
@@ -0,0 +1,45 @@
+package bugbot.listeners;
+
+import bugbot.config.Config;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.kohsuke.github.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public class BugHandler extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        StringBuilder message = new StringBuilder();
+
+        if(event.getTextChannel().getId().contains("771863258066845736"))
+            message.append("[CANARY] ");
+        else if(event.getTextChannel().getId().contains("707243949672235058"))
+            message.append("[SUGGESTION] ");
+        else if(event.getTextChannel().getId().contains("707243764586119248"))
+            message.append("[MAIN] ");
+        else
+            return;
+
+        message.append(event.getMessage().getContentDisplay());
+
+        try {
+            GitHub github = new GitHubBuilder().withOAuthToken(Config.ACCESS_TOKEN,Config.ORG_NAME).build();
+
+            List<GHProject> prs = github.getRepository("namespace-media/calmY-XF").listProjects(GHProject.ProjectStateFilter.OPEN).toList();
+            List<GHProjectColumn> colms = prs.get(2).listColumns().toList();
+            colms.get(0).createCard(
+                     message.toString()
+            );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        event.getMessage().addReaction(Objects.requireNonNull(event.getJDA().getEmoteById("792577321109422111"))).queue();
+    }
+}
