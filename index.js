b29a3fea207decade16f1d6531582bcb55195b0b
ConCode <concode@outlook.de>
2021-04-30 02:01:15 +0200

    Reworked BugBot in JS and made it dynamic

---
+++ index.js
@@ -0,0 +1,68 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+let aliases = require(`./aliases.json`)
+const supportRole = `707246903003447357`;
+
+const client = new Discord.Client();
+
+client.on('message', message => {
+	if (message.author.bot) return;
+	if (!message.member.roles.cache.has(supportRole)) return;
+	let prefix = `/`;
+
+	if(!message.content.startsWith(prefix)) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	switch (command) {
+		case `alias`:
+			if (args.length == 2 && message.mentions.members.array().length == 1) {
+				const mentioned = message.mentions.members.last();
+				aliases[mentioned.id] = args[1];
+				message.channel.send(new Discord.MessageEmbed().setDescription(`Set ${mentioned}'s alias to **${args[1]}**.`));
+				fs.writeFileSync(`./aliases.json`, JSON.stringify(aliases));
+			}
+			break;
+
+		case `delete`:
+			message.guild.fetch();
+			const channels = message.guild.channels.cache.array();
+			channels.forEach(element => {
+				if (element.name.startsWith(`closed-`))
+					element.delete();
+			});
+			break;
+
+		case `close`:
+			if (args.length == 1 && message.mentions.members.array().length == 1) {
+				const mentioned = message.mentions.members.last();
+				message.channel.send(`Hey ${mentioned}, if you still need support please specify your problem here!\nOtherwise, we ask you to close this ticket above by pressing the lock and then the green tick to confirm.\n\nKind regards,\nyour XP Team`)
+			}
+			break;
+
+		default:
+			message.channel.send(new Discord.MessageEmbed().setDescription(`\`\`\`md\n${prefix}alias <member> <alias>\n${prefix}delete\n${prefix}close <member>\`\`\``).setTitle(`Usages`));
+			break;
+	}
+	// other commands...
+});
+
+client.on('message', async (message) => {
+	if (message.author.bot) return;
+	if (!(message.channel.name.startsWith(`closed-`) && !isNaN(message.channel.name.slice(7).trim()))) return;
+	await message.member.fetch();
+	if (!message.member.roles.cache.has(supportRole)) return;
+	if (message.channel.type != `text`) return;
+
+	let title = message.channel.name;
+	if (aliases[message.member.id]) {
+		title += `-${aliases[message.member.id]}`
+	} else {
+		title += `-${message.member.user.username}`
+	}
+	message.channel.setName(title);
+	message.channel.send(title);
+});
+
+client.login(`NzkzNjQ3OTM0MDg0MDg3ODA4.X-vUPA.ngTsAE48gRgy8blvyc7s2_5ITFE`);
--- src/main/java/bugbot/config/Config.java
+++
@@ -1,9 +0,0 @@
-package bugbot.config;
-
-public class Config {
-
-    public static String TOKEN = "NzkzNjQ3OTM0MDg0MDg3ODA4.X-vUPA.ngTsAE48gRgy8blvyc7s2_5ITFE";
-    public static String ACCESS_TOKEN = "fb501930cbb002f000357c2efa52a2a221686b25";
-    public static String ORG_NAME = "namespace-media";
-
-}
