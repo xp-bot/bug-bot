8f4fc0f67b634eb094e47eb2e25bfd58b66c861c
conny <36863945+ConnysCode@users.noreply.github.com>
2022-06-15 00:31:14 +0200

    Added FAQs

--- src/commands/rFaq.ts
+++ src/commands/rFaq.ts
@@ -18,11 +18,23 @@ const slash: SlashCommandBuilder | any = new SlashCommandBuilder()
           name: `How do I make it so only the highest level role gets applied?`,
           value: `srr`
         },
-        { name: `How do I access the dashboard?`, value: `dashboard` },
+        {
+          name: `Why are XPs Slash Commands not working on my Community?`,
+          value: `slashnotworking`
+        },
+        {
+          name: `I bought Server premium. Why do i still not have the features?`,
+          value: `activatepremium`
+        },
+        {
+          name: `What is the best way to support you guys?`,
+          value: `supportus`
+        },
         { name: `other...`, value: `other` }
       )
       .setName(`question`)
-      .setDescription(`Select a question!`).setRequired(true)
+      .setDescription(`Select a question!`)
+      .setRequired(true)
   )
   .setDescription(`Some good to know things.`);
 module.exports = {
@@ -43,8 +55,9 @@ async function execute(interaction: CommandInteraction) {
       embed
         .setTitle("How do I add the level to someone's nickname?")
         .setDescription(
-          'Enable the Autonick module in the Dashboard.\> https://xp-bot.net/me/servers'
-        ).setImage(`https://img.namespace.media/images/msedge_T5LelagevY.png`);
+          'Enable the Autonick module in the Dashboard.\n> https://xp-bot.net/me/servers'
+        )
+        .setImage(`https://img.namespace.media/images/msedge_T5LelagevY.png`);
       break;
     }
     case 'srr': {
@@ -53,8 +66,63 @@ async function execute(interaction: CommandInteraction) {
           'How do I make it so only the highest level role gets applied?'
         )
         .setDescription(
-          'Enable the Single Rank Role module in the Dashboard.\> https://xp-bot.net/me/servers'
-        ).setImage(`https://img.namespace.media/images/msedge_8Kde3e38bc.png`);
+          'Enable the Single Rank Role module in the Dashboard.\n> https://xp-bot.net/me/servers'
+        )
+        .setImage(`https://img.namespace.media/images/msedge_8Kde3e38bc.png`);
+      break;
+    }
+    case 'slashnotworking': {
+      embed
+        .setTitle('Why are XPs Slash Commands not working on my Community?')
+        .setDescription(
+          "Since you probably invited XP prior to us introducing Slash Commands, you may not have authorized XP to register them.\nTo fix this, you can simply reinvite XP. You don't even have to kick XP first!"
+        )
+        .setImage(
+          `https://media.discordapp.net/attachments/841338904899878912/986352612985163877/unknown.png`
+        );
+      break;
+    }
+    case 'activatepremium': {
+      embed
+        .setTitle(
+          'I bought Server premium. Why do i still not have the features?'
+        )
+        .setDescription(
+          `First of all. Thank you!
+          Even the smallest financial support moves worlds here.
+          
+          You can select - and activate your premium servers in your User Settings:
+          https://xp-bot.net/me/premium
+          
+          _Once it lights up blue, its active! No need for seperate saving._`
+        )
+        .setImage(`https://img.namespace.media/images/activatepremium.png`);
+      break;
+    }
+    case 'supportus': {
+      embed
+        .setTitle(
+          'What is the best way to support you guys?'
+        )
+        .setDescription(
+          'First of all. Thank you!\nEven the smallest financial support moves worlds here.\n\nIf you want to support XP, you have to become a Premium user.\nYou can do this through Patreon and get some benefits that are exclusive to Premium users.'
+        )
+        .addField(
+          `User Premium Benefits`,
+          `**-** Premium Badge on your Ranking Card <:xppremium:851213381611946025>\n**-** You can have bigger Backgrounds and Blur them\n**-** You'll never have to vote again`,
+          false
+        )
+        .addField(
+          `Server Premium Benefits`,
+          `**-** All of the above +\n**-** Autonick customization\n**-** Remove reached Levelroles\n**-** Reset left Users\n**-** Use commands in Threads\n**-** Custom Game Cooldowns\n**-** Custom max. Daily XP\n**-** Logs in your Dashboard\n**-** Custom Leaderboard Background\n**-** Dynamic Discord Leaderboard`,
+          false
+        )
+        .addField(
+          `Become a Premium User`,
+          `[Patreon](https://premium.xp-bot.net/)`,
+          false
+        )
+        .setImage(`https://img.namespace.media/images/2021/06/16/bsp3.png`);
       break;
     }
     case 'other':
--- src/listeners/closeTicketListener.ts
+++ src/listeners/closeTicketListener.ts
@@ -1,4 +1,10 @@
-import { ButtonInteraction, MessageEmbed } from 'discord.js';
+import {
+  ButtonInteraction,
+  MessageActionRow,
+  MessageEmbed,
+  Modal,
+  TextInputComponent
+} from 'discord.js';
 
 export default async function closeTicketListener(
   interaction: ButtonInteraction
@@ -19,16 +25,55 @@ export default async function closeTicketListener(
     return;
   }
 
-  interaction
-    .reply({
-      embeds: [
-        new MessageEmbed()
-          .setTitle(`Ticket closed`)
-          .setDescription(`${interaction.user} closed the Ticket!`)
-          .setColor(`#52D94F`)
-      ]
-    })
-    .catch((err) => {});
+  // const resolveComponent = new TextInputComponent()
+  //   .setCustomId(`tInterviewResolve`)
+  //   .setLabel(`Could we help you resolve your issue?`)
+  //   .setStyle(`SHORT`);
+  // const satisfiedComponent = new TextInputComponent()
+  //   .setCustomId(`tInterviewSatisfied`)
+  //   .setLabel(`How satisfied were you with our service?`)
+  //   .setPlaceholder(`1 - 5 (1 = Terrible / 5 = Perfect)`)
+  //   .setStyle(`SHORT`);
+
+  // const improveComponent = new TextInputComponent()
+  //   .setCustomId(`tInterviewImprove`)
+  //   .setLabel(`Help us improve!`)
+  //   .setPlaceholder(
+  //     `What's on your mind? This Message can help you to improve future Support Cases.`
+  //   )
+  //   .setStyle(`PARAGRAPH`);
+  // const modal = new Modal()
+  //   .setCustomId('ticketInterviewModal')
+  //   .setTitle('How did we do?')
+  //   .addComponents(new MessageActionRow().addComponents(resolveComponent));
+  // interaction.showModal(modal);
+
+  // const favoriteColorInput = new TextInputComponent()
+  //   .setCustomId('favoriteColorInput')
+  //   // The label is the prompt the user sees for this input
+  //   .setLabel("What's your favorite color?")
+  //   // Short means only a single line of text
+  //   .setStyle('SHORT');
+  // const hobbiesInput = new TextInputComponent()
+  //   .setCustomId('hobbiesInput')
+  //   .setLabel("What's some of your favorite hobbies?")
+  //   // Paragraph means multiple lines of text.
+  //   .setStyle('PARAGRAPH');
+  // // An action row only holds one text input,
+  // // so you need one action row per text input.
+  // const firstActionRow = new MessageActionRow().addComponents(
+  //   favoriteColorInput
+  // );
+  // const secondActionRow = new MessageActionRow().addComponents(hobbiesInput);
+
+  interaction.reply({
+    embeds: [
+      new MessageEmbed()
+        .setTitle(`Ticket closed`)
+        .setDescription(`${interaction.user} closed the Ticket!`)
+        .setColor(`#52D94F`)
+    ]
+  });
   if (interaction.channel?.type === `GUILD_TEXT`) {
     interaction.channel.setParent(config.archiveCategory, {
       lockPermissions: false
