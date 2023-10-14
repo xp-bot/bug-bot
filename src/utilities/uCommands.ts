f3854a7fa933d79d8f3e5c8c8303c332d88777f1
ClearMist <cringleysdays@outlook.com>
2023-10-14 17:38:10 +0100

    Changed to using discord.js for REST and Routes

--- src/utilities/uCommands.ts
+++ src/utilities/uCommands.ts
@@ -1,5 +1,4 @@
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
+import { REST } from "discord.js";
 import fs from "fs";
 import path from "path";
 import { CommandInterface } from '../interfaces/internalInterfaces';
